45c91a20f1b95155b5509c68c605593d43e92459
佐藤克樹 <k-sato@ruby-dev.jp>
2019-06-06 08:38:23 +0900

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import StateHooks from "./stateHooks/index";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>React Hooks</h1>
+      <StateHooks />
+    </div>
+  );
+};
+
+export default App;
