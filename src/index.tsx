5210abd0cc229c45653591ae39ac9065a18bedc1
Katsuki Sato <katsuki_sato0130@icloud.com>
2023-07-04 13:59:52 +0900

    tttt

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 import TextProvider from "./components/contextHooks/TextProvider";
 
+console.log('hello world')
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
